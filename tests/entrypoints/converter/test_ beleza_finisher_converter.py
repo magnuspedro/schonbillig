8bb192d5c7831cab2d64301490b665dd78163141
Magnus <pedmagnus@gmail.com>
2021-02-09 23:14:39 -0300

    creating testing for all converters

---
+++ tests/entrypoints/converter/test_ beleza_finisher_converter.py
@@ -0,0 +1,50 @@
+from src.entities.shampoo import Shampoo
+from src.entrypoints.converter.get_shampoo_beleza_converter import GetShampooBelezaConverter
+from tests.fixtures.beleza_product_fixture import BelezaProductFixture
+
+
+def test_converter_shampoo(mocker):
+    html = BelezaProductFixture(name='Wella Professionals Fusion',
+                                price='555.059,90',
+                                brand='Wella Professionals',
+                                brand_line='Fusion',
+                                hair_type='Danificados',
+                                size_unit='Miniatura',
+                                utility='Força e Resistência',
+                                hair_shaft_condition='Quebradiços',
+                                texture='Liquido')
+    request = mocker.Mock()
+    request.text = str(html)
+    request.url = 'https://www.belezanaweb.com.br/wella-professionals-fusion-shampoo-50ml/'
+    shampoo = GetShampooBelezaConverter().to_entity(request)
+    assert type(shampoo) == Shampoo
+
+
+def test_converter_attributes_shampoo(mocker):
+    html = BelezaProductFixture(name='Wella Professionals Fusion',
+                                price='555.059,90',
+                                brand='Wella Professionals',
+                                brand_line='Fusion',
+                                hair_type='Danificados',
+                                size_unit='Miniatura',
+                                utility='Força e Resistência',
+                                hair_shaft_condition='Quebradiços',
+                                texture='Liquido')
+    request = mocker.Mock()
+    request.text = str(html)
+    request.url = 'https://www.belezanaweb.com.br/wella-professionals-fusion-shampoo-50ml/'
+    shampoo = GetShampooBelezaConverter().to_entity(request)
+    assert shampoo.name == 'Wella Professionals Fusion'
+    assert shampoo.brand == 'Wella Professionals'
+    assert shampoo.brand_line == 'Fusion'
+    assert shampoo.price[0].price == 555059.90
+    assert shampoo.vegan == False
+    assert shampoo.size == '50ml'
+    assert shampoo.utility == 'Força e Resistência'
+    assert shampoo.size_unit == 'Miniatura'
+    assert shampoo.hair_type == 'Danificados'
+    assert shampoo.hair_shaft_condition == 'Quebradiços'
+    assert shampoo.url[0].string == 'https://www.belezanaweb.com.br/wella-professionals-fusion-shampoo-50ml/'
+    assert shampoo.url[0].source == 'belezanaweb'
+    assert shampoo.code[0].code == '52110'
+    assert shampoo.texture == 'Liquido'
---
+++ tests/fixtures/beleza_product_fixture.py
@@ -0,0 +1,102 @@
+class BelezaShampooFixture:
+
+    def __init__(self, name, price, brand, brand_line, hair_type, size_unit, utility, hair_shaft_condition):
+        self.name = name
+        self.price = price
+        self.brand = brand
+        self.brand_line = brand_line
+        self.hair_type = hair_type
+        self.size_unit = size_unit
+        self.utility = utility
+        self.hair_shaft_condition = hair_shaft_condition
+
+    def __str__(self):
+        string = '''<h1 class="nproduct-title">
+                ''' + self.name + '''
+                    <span class="product-subtitle">  - Shampoo 50ml</span>
+            </h1>
+        <div class="product-sku">
+                    <strong>Cod: </strong>52110
+                </div>
+        <div class="container-padding product-datasheet container-padding-top card-xs">
+            <div class="accordion-gradient">
+                <input type="checkbox" class="is-hide accordion-state" id="product-datasheet" checked="">
+                <div class="accordion-gradient-content datasheet">
+                    <div class="datasheet-categories"> 
+                        <span class="info-label">Categorias</span>
+                        <div>
+                            <span class="info-link info-link-category">
+                                <a href="/cabelos" data-interaction="{&quot;category&quot;: &quot;PRODUCTS&quot;, &quot;action&quot;: &quot;Link-Click&quot;, &quot;label&quot;: &quot;datasheet department&quot;, &quot;values&quot;: &quot;department;cabelos&quot;}">
+                                    Cabelos
+                                </a>
+                            </span>
+                                <span class="info-link info-link-category">
+                                    <a href="/cabelos/shampoo" data-interaction="{&quot;category&quot;: &quot;PRODUCTS&quot;, &quot;action&quot;: &quot;Link-Click&quot;, &quot;label&quot;: &quot;datasheet category&quot;, &quot;values&quot;: &quot;category;cabelos/shampoo&quot;}">
+                                        Shampoo
+                                    </a>
+                                </span>
+                        </div>
+                    </div>
+                    <div class="datasheet-characteristics">
+                                <div class="info-line">
+                                    <span class="info-label">Tipos de Cabelo</span>
+                                                <strong class="info-link">
+                                                    <a href="/cabelos/shampoo/danificados" data-interaction="{&quot;category&quot;: &quot;PRODUCTS&quot;, &quot;action&quot;: &quot;Link-Click&quot;, &quot;label&quot;: &quot;datasheet attribute&quot;,  &quot;values&quot;: &quot;attribute;/cabelos/shampoo/danificados&quot;}">
+                                                        ''' + self.hair_type + '''
+                                                    </a>
+                                                </strong>       
+                                </div>
+                                <div class="info-line">
+                                    <span class="info-label">Condição dos Fios</span>
+                                                <strong class="info-link">
+                                                    <a href="/cabelos/shampoo/danificados?condicao-dos-fios=quebradicos" data-interaction="{&quot;category&quot;: &quot;PRODUCTS&quot;, &quot;action&quot;: &quot;Link-Click&quot;, &quot;label&quot;: &quot;datasheet attribute&quot;,  &quot;values&quot;: &quot;attribute;/cabelos/shampoo/danificados?condicao-dos-fios=quebradicos&quot;}">
+                                                        ''' + self.hair_shaft_condition + '''
+                                                    </a>
+                                                </strong>
+                                </div>
+                                <div class="info-line">
+                                    <span class="info-label">Desejo de Beleza</span>
+                                                <span class="info-value">
+                                                    ''' + self.utility + '''
+                                                </span>
+                                </div>
+                                <div class="info-line">
+                                    <span class="info-label">Tamanho</span>
+                                                <strong class="info-link">
+                                                    <a href="/cabelos/shampoo/danificados?tamanho=miniatura" data-interaction="{&quot;category&quot;: &quot;PRODUCTS&quot;, &quot;action&quot;: &quot;Link-Click&quot;, &quot;label&quot;: &quot;datasheet attribute&quot;,  &quot;values&quot;: &quot;attribute;/cabelos/shampoo/danificados?tamanho=miniatura&quot;}">
+                                                        ''' + self.size_unit + '''
+                                                    </a>
+                                                </strong>
+                               </div>                  
+                            <div class="info-line">
+                                <span class="info-label">Marca</span>
+                                <strong class="info-link">
+                                    <a href="/wella-professionals/cabelos/shampoo" data-interaction="{&quot;category&quot;: &quot;PRODUCTS&quot;, &quot;action&quot;: &quot;Link-Click&quot;, &quot;label&quot;: &quot;datasheet brand wella-professionals/cabelos/shampoo&quot;}">
+                                        ''' + self.brand + '''
+                                    </a>
+                                </strong>
+                            </div>
+                            <div class="info-line">
+                                <span class="info-label">Linha</span>
+                                <strong class="info-link">
+                                    <a href="/wella-professionals/fusion" data-interaction="{&quot;category&quot;: &quot;PRODUCTS&quot;, &quot;action&quot;: &quot;Link-Click&quot;, &quot;label&quot;: &quot;datasheet brand line wella-professionals/fusion&quot;}">
+                                        ''' + self.brand_line + '''
+                                    </a>
+                                </strong>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="nprodct-price">     
+            <strong class="nproduct-price-max">
+                <s>R$ 555.059,90</s>
+    <span class="" data-href="https://www.belezanaweb.com.br/wella-professionals-fusion-shampoo-50ml/">
+        (17% de desconto)
+    </span>
+            </strong>
+        <div class="nproduct-price-value" content="49.9">
+            R$ ''' + self.price + '''
+        </div>
+    </div>'''
+        return string
